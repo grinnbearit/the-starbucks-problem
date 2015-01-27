1e823bc16c5d65e2694bf7032495acef80a53d81
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2015-01-27 12:47:50 +0530

    starbucks probability

--- src/the_starbucks_problem/core.clj
+++ src/the_starbucks_problem/core.clj
@@ -1,6 +1,34 @@
 (ns the-starbucks-problem.core
-  (:require [the-starbucks-problem.api :as api]))
+  (:require [the-starbucks-problem.api :as api]
+            [the-starbucks-problem.geometry :refer [passes?]]))
 
 
 (def FACTUAL [33.8790167 -118.2339481])
-(def LAX [33.9425, -118.4081])
+(def LAX [33.9425 -118.4081])
+
+
+(defn starbucks-step
+  [step locations]
+  (filter #(passes? [(:start step) (:end step)] (:location %)) locations))
+
+
+(defn starbucks-route
+  [route]
+  (let [locations (api/starbucks (get-in route [:bounds :top-left])
+                                 (get-in route [:bounds :bottom-right]))]
+    (for [step (:steps route)
+          :let [passed-bucks (starbucks-step step locations)]
+          :when (seq passed-bucks)]
+      (assoc step :starbucks passed-bucks))))
+
+
+(defn starbucks-probability
+  [origin destination minutes]
+  (let [all-routes (api/directions origin destination)
+        valid-routes (for [route all-routes
+                           :when (<= (:duration route) (* minutes 60))
+                           :let [steps (starbucks-route route)]
+                           :when (seq steps)]
+                       (assoc route :starbucks steps))]
+    {:probability (/ (count valid-routes) (count all-routes))
+     :valid-routes valid-routes}))
--- test/the_starbucks_problem/core_test.clj
+++ test/the_starbucks_problem/core_test.clj
@@ -1,1 +1,71 @@
-(ns the-starbucks-problem.core-test)
+(ns the-starbucks-problem.core-test
+  (:require [midje.sweet :refer :all]
+            [the-starbucks-problem.core :refer :all]
+            [the-starbucks-problem.geometry :as g]
+            [the-starbucks-problem.api :as api]))
+
+
+(facts
+ (starbucks-step {:start [0 0] :end [10 0]}
+                 [{:location [2 0.5]}
+                  {:location [2 -10]}])
+ => [{:location [2 0.5]}]
+
+ (provided
+  (g/passes? [[0 0] [10 0]] [2 0.5]) => true
+  (g/passes? [[0 0] [10 0]] [2 -10]) => false))
+
+
+(facts
+ (starbucks-route {:bounds {:top-left [0 10]
+                            :bottom-right [10 0]}
+                   :steps [{:start [0 0] :end [10 0]}
+                           {:start [10 0] :end [10 10]}]})
+ => [{:start [0 0]
+      :end [10 0]
+      :starbucks [{:location [2 0.5]}]}]
+
+ (provided
+  (api/starbucks [0 10] [10 0])
+  => [{:location [2 0.5]}
+      {:location [2 -10]}]
+
+  (starbucks-step {:start [0 0] :end [10 0]}
+                  [{:location [2 0.5]}
+                   {:location [2 -10]}])
+  => [{:location [2 0.5]}]
+
+  (starbucks-step {:start [10 0] :end [10 10]}
+                  [{:location [2 0.5]}
+                   {:location [2 -10]}])
+  => []))
+
+
+(facts
+ (starbucks-probability [0 0] [10 10] 5)
+ => {:probability 1/2
+     :valid-routes [{:bounds {:bottom-right [10 0]
+                              :top-left [0 10]}
+                     :duration 180
+                     :steps [{:start [0 0] :end [10 10]}]
+                     :starbucks [{:start [0 0] :end [10 0]
+                                  :starbucks [{:location [5 4]}]}]}]}
+
+ (provided
+  (api/directions [0 0] [10 10])
+  => [{:bounds {:top-left [0 10]
+                :bottom-right [10 0]}
+       :duration 180
+       :steps [{:start [0 0] :end [10 10]}]}
+      {:bounds {:top-left [0 10]
+                :bottom-right [10 0]}
+       :duration 420
+       :steps [{:start [0 0] :end [0 10]}
+               {:start [0 10] :end [10 10]}]}]
+
+  (starbucks-route {:bounds {:top-left [0 10]
+                             :bottom-right [10 0]}
+                    :duration 180
+                    :steps [{:start [0 0] :end [10 10]}]})
+  => [{:start [0 0] :end [10 0]
+       :starbucks [{:location [5 4]}]}]))
